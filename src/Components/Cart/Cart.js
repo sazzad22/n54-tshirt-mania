13d4286df967c759ca39e9bc66e0f2dc708bb3eb
Sazzad ur Rahman <22joylobo@gmail.com>
2022-04-03 15:19:09 +0600

    tShirt SPA store initiated

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div>
+            <h5>`Cart</h5>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/Components/OrderReview/OrderReview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h3>OrderReview</h3>
+        </div>
+    );
+};
+
+export default OrderReview;
